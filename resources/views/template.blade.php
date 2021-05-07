1c17324df31ccd73d0830b0b56ab888d9dd7c47f
Tsekko <maxime.kervadec@laposte.net>
2021-05-07 09:59:45 +0200

    Modif NavBar auth P1

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -44,9 +44,10 @@
                 <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
             </div>
             <div class="hidden sm:block sm:ml-6">
+            @if (Route::has('login'))
                 <div class="flex space-x-4">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    
+                    @auth
                     <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
 
                     <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
@@ -54,8 +55,11 @@
                     <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
                     <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                    @else
                     <p class="text-white">Non connecté</p>
+                    @endauth
                 </div>
+            @endif
             </div>
         </div>
         <div @click.away="open = false" class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0" x-data="{ open: false }">
