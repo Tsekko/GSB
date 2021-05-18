8d19b49a6e837591fdd80f737c7c323cf71eff96
Tsekko <maxime.kervadec@laposte.net>
2021-05-18 14:32:35 +0200

    Modif DB et création vueProfil

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,26 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('VIS_MATRICULE')->length(10);
+            $table->string('VIS_NOM')->length(25);
+            $table->string('VIS_PRENOM')->length(50);
+            $table->string('VIS_ADRESSE')->length(50);
+            $table->string('VIS_CP')->length(5);
+            $table->string('VIS_VILLE')->length(30);
+            $table->date('VIS_DATEEMBAUCHE');
+            $table->string('SEC_CODE')->length(1)->nullable();
+            $table->string('LAB_CODE')->length(2);
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('SEC_CODE')->references('SEC_CODE')->on('secteur');
+            $table->foreign('LAB_CODE')->references('LAB_CODE')->on('labo');
+
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
         });
     }
 
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,7 @@
+@extends('template')
+
+@section('title', 'Mon profil')
+
+@section('content')
+
+@endsection
