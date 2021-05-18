b4d825ddc3e84293dce99354abdcb54ce6753c46
Tsekko <maxime.kervadec@laposte.net>
2021-05-18 17:35:53 +0200

    Affichage des infos de l'utilisateur

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'id',
+        'VIS_MATRICULE',
+        'VIS_NOM',
+        'VIS_PRENOM',
+        'VIS_ADRESSE',
+        'VIS_CP',
+        'VIS_VILLE',
+        'VIS_DATEEMBAUCHE',
+        'SEC_CODE',
+        'LAB_CODE',
         'email',
         'password',
     ];
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,5 +3,41 @@
 @section('title', 'Mon profil')
 
 @section('content')
-
+@foreach($ConnectedUsers as $user)
+<table class="table-auto">
+  <thead>
+    <tr>
+      <th>Profil</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>Nom</th>
+      <td>{{ $user->VIS_NOM }}</td>
+    </tr>
+    <tr class="bg-emerald-200">
+      <th>Prénom</th>
+      <td>{{ $user->VIS_PRENOM }}</td>
+    </tr>
+    <tr>
+      <th>Adresse</th>
+      <td>{{ $user->VIS_ADRESSE }} {{ $user->VIS_CP }} {{ $user->VIS_VILLE }}</td>
+    </tr>
+    <tr>
+      <th>Date d'embauche</th>
+      <td>{{ $user->VIS_DATEEMBAUCHE }}</td>
+    </tr>
+    @if($user->SEC_CODE!= null)
+    <tr>
+      <th>Code du secteur</th>
+      <td>{{ $user->SEC_CODE }}</td>
+    </tr>
+    @endif
+    <tr>
+      <th>Code du laboratoire</th>
+      <td>{{ $user->LAB_CODE }}</td>
+    </tr>
+  </tbody>
+</table>
+@endforeach
 @endsection
